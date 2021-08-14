c121e609b9c87722199e0e2e78b65b2ab9aa0fc9
Olena <hbrysh95@gmail.com>
2021-08-14 21:45:52 +0200

    added all required basic files & set js logic that allows rendering each menu item on the page

---
+++ index.js
@@ -0,0 +1,89 @@
+const menuArray = [
+  {
+    id: 1,
+    title: "Softest pancakes",
+    category: "breakfast",
+    price: 13.99,
+    img: "./img/item-1.jpeg",
+    description: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "Double diner",
+    category: "lunch",
+    price: 15.99,
+    img: "./img/item-2.jpeg",
+    description: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing.`,
+  },
+  {
+    id: 3,
+    title: "Strawberry milkshake",
+    category: "shakes",
+    price: 7.99,
+    img: "./img/item-3.jpeg",
+    description: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "Cheesy egg toast",
+    category: "breakfast",
+    price: 17.99,
+    img: "./img/item-4.jpeg",
+    description: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "Kingsize burger",
+    category: "lunch",
+    price: 22.99,
+    img: "./img/item-5.jpeg",
+    description: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "Chocolate milkshake",
+    category: "shakes",
+    price: 8.99,
+    img: "./img/item-6.jpeg",
+    description: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "Bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "./img/item-7.jpeg",
+    description: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "American classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./img/item-8.jpeg",
+    description: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  }
+];
+
+const menuContainer = document.querySelector(".menu-container");
+window.addEventListener("DOMContentLoaded", function () {
+  let showMenuItems = menuArray.map(function (item) {
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="item-image" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.description}
+            </p>
+          </div>
+        </article>`;
+
+  });
+  showMenuItems = showMenuItems.join("");
+
+  menuContainer.innerHTML = showMenuItems;
+});
